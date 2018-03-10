6a2fed3729b7673f41bb0c240f1a0d1b72c1b1cb
Giovani Aguirre <aguirregio19@gmail.com>
2018-03-09 16:08:52 -0800

    Adding chained get request for different cases

--- 08_Express_Start/server.js
+++ 08_Express_Start/server.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-})
+app
+  .get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>");
+  })
+  .get("/profile", (req, res) => {
+    const profile = {name: "Gio"}
+    res.send(profile);
+  })
 
 app.listen(3000)
